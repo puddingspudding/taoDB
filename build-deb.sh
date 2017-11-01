6200bb0027953d39fc90b4ec87aa206327ac17d7
Pudding <philip.strecker@googlemail.com>
2017-11-01 21:36:57 +0100

    fixed paths for travis builds

--- build-deb.sh
+++ build-deb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mvn clean package \
-&& mv target/tao-db-1.0-SNAPSHOT-jar-with-dependencies.jar src/main/deb/usr/lib/taodb/taodb.jar \
-&& cd src/main/deb/ \
+&& mv /home/travis/build/puddingspudding/taoDB/target/tao-db-1.0-SNAPSHOT-jar-with-dependencies.jar /home/travis/build/puddingspudding/taoDB/src/main/deb/usr/lib/taodb/taodb.jar \
+&& cd /home/travis/build/puddingspudding/taoDB/src/main/deb/ \
 && dpkg -b ./ taodb.deb
 
 
