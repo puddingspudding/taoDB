81fed0692eb37844a9ccba9ff7274ca34c69099f
Pudding <philip.strecker@googlemail.com>
2017-11-08 23:08:57 +0100

    fixed travis build

--- build-deb.sh
+++ build-deb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mvn clean package \
 && mkdir -p /home/travis/build/puddingspudding/taoDB/src/main/deb/usr/lib/taodb \
-&& mv /home/travis/build/puddingspudding/taoDB/target/tao-db-1.0-SNAPSHOT-jar-with-dependencies.jar /home/travis/build/puddingspudding/taoDB/src/main/deb/usr/lib/taodb/taodb.jar \
+&& mv /home/travis/build/puddingspudding/taoDB/target/taodb-*-jar-with-dependencies.jar /home/travis/build/puddingspudding/taoDB/src/main/deb/usr/lib/taodb/taodb.jar \
 && cd /home/travis/build/puddingspudding/taoDB/src/main/deb/ \
 && dpkg -b ./ taodb.deb
 
